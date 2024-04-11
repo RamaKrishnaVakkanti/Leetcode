854f676d6d7a8af3c24eaaad1f353a9d048371f9
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-10 20:28:44 -0500

    Time: 49 ms (84.37%), Space: 50.7 MB (55.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,8 +3,6 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    s = s.trim();
     return (s.split(" ").filter((str)=>str !="").reverse().join(" "));
-    // console.log(s.split(" ").filter((str)=>str !=""))
     
 };

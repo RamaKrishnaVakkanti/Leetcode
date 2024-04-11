3238a2c789e11359eef0d86c4d7bf97fe4fb1cc9
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-10 19:51:22 -0500

    Time: 69 ms (11.33%), Space: 50.8 MB (26.87%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -5,6 +5,7 @@
  */
 var gcdOfStrings = function(str1, str2) {
     output = "";
+    if(str1 == str2) return str1;
     let smallest = str1.length<str2.length ? str1 : str2;
     for(let i=0;i<smallest.length;i++){
         if(str1.replaceAll(str1.slice(0,i+1),"")== "" && str2.replaceAll(str1.slice(0,i+1),"") == ""){

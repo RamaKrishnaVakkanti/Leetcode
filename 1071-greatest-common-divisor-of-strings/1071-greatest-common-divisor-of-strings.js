1ceed40b2dd3613d1cea18dd6b322b9f3542377a
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-10 19:45:18 -0500

    Time: 83 ms (6.23%), Space: 53.1 MB (8.46%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    output = "";
+    let largest = str1.length>str2.length ? str1 : str2;
+    for(let i=0;i<largest.length;i++){
+        if(str1.replaceAll(str1.slice(0,i+1),"")== ""){
+            
+            if(str2.replaceAll(str1.slice(0,i+1),"") == ""){
+                output=  str1.slice(0,i+1);
+            }
+        }
+    }
+    return output;
+};

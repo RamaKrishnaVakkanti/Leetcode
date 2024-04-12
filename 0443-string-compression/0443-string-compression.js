49f8f7036b5d42cfb13c72763c2fb2a74d6a6601
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-11 19:43:53 -0500

    Time: 54 ms (91.35%), Space: 51.6 MB (66.91%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,24 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    
+    let count =1;
+    let output = chars[0];
+    
+    for(let i=1;i<chars.length;i++){
+        if(chars[i] === chars[i-1]) count++;
+        else{
+            output=count>1 ? output+count+chars[i] : output+chars[i];
+            count=1;
+        }
+    }
+    if(count>1) output = output+count;
+    chars.length=0;
+    for(let char of output){
+        chars.push(char);
+    }
+    chars = output.split("");
+    console.log(output);
+};

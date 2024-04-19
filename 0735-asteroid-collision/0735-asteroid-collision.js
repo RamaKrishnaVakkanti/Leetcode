b5d8f5574a7581ff7121e994b20add87875af07c
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-18 20:19:33 -0500

    Time: 68 ms (33.17%), Space: 50.8 MB (92.36%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    
+//     let stack = [asteroids[0]];
+//     let length = asteroids.length;
+//     for(let i=1;i<length;i++){
+//         let newAst = asteroids[i];
+//         let lastAst = stack.at(-1);
+//         while(lastAst && (newAst>0 && lastAst<0) || (newAst<0 && lastAst>0)){
+//             stack.pop();
+//             newAst = last
+            
+//         }
+//     }
+    
+    let asteroidsStack =[];
+    
+    for(let i=0;i<asteroids.length;i++){
+        let lastAst = asteroidsStack.at(-1);
+        let currentAst = asteroids[i];
+        
+        if((lastAst>0 && currentAst<0) ){
+            if(lastAst+currentAst === 0){
+                asteroidsStack.pop();
+            }else if(Math.abs(lastAst)<Math.abs(currentAst)){
+                asteroidsStack.pop();
+                i--;
+            }
+        }else{
+            asteroidsStack.push(currentAst);
+        }
+    }
+    return asteroidsStack;
+};

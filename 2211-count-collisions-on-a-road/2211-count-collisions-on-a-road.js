b92460861cd5b29377a2ce4b247489633035961d
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-19 20:11:13 -0500

    Time: 105 ms (33.33%), Space: 63.9 MB (18.52%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.js
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.js
@@ -1,33 +1,23 @@
-/**
- * @param {string} directions
- * @return {number}
- */
 var countCollisions = function(directions) {
-    directions = directions.split("");
-    let vehicleStack = [];
-    let output = 0;
-    
-    for(let i=0;i<directions.length;i++){
-        
+    let directionsArray = directions.split(""), vehicleStack = [], output = 0;
+    for (let i = 0; i < directionsArray.length; i++) {
         let lastVehicle = vehicleStack.at(-1);
-        let currentVehicle = directions[i];
-        
-        if(currentVehicle == 'L'){
-            if(lastVehicle == 'R'){
-                output+=2;
-                vehicleStack.pop();
-                directions[i]='S';
-                i--;
-            }else if(lastVehicle =='S'){
-                output+=1;
-            }
-        }else if(currentVehicle == 'S' && lastVehicle == 'R'){
-            output+=1;
+        let currentVehicle = directionsArray[i];
+        if (currentVehicle == 'L' && lastVehicle == 'R') {
+            //In this case both are moving in oposite Direction, Collision will happens.
+            output += 2;
+            // Removing the last "R" and update the current index vlue to "S", because after collision both vehicle will be in stable ("S") state
+            vehicleStack.pop();
+            directionsArray[i] = 'S';
+            // Moving index to back to check this again with the top element in the vehicle stack.
+            i--;
+        } else if (currentVehicle == 'L' && lastVehicle == 'S') output += 1;
+        else if (currentVehicle == 'S' && lastVehicle == 'R') {
+            // Here collision will happen and we also need to check with previous elements in the stack.
+            output += 1;
             vehicleStack.pop();
             i--;
-        }else{
-            vehicleStack.push(currentVehicle);
-        }        
+        } else vehicleStack.push(currentVehicle);
     }
     return output;
 };

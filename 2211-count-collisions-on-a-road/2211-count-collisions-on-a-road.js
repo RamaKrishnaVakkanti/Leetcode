7d08490a1162566899bc3287f11daac4b7d4bb3f
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-04-19 19:49:40 -0500

    Time: 103 ms (33.33%), Space: 61.7 MB (18.52%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.js
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.js
@@ -6,7 +6,9 @@ var countCollisions = function(directions) {
     directions = directions.split("");
     let vehicleStack = [];
     let output = 0;
+    
     for(let i=0;i<directions.length;i++){
+        
         let lastVehicle = vehicleStack.at(-1);
         let currentVehicle = directions[i];
         

16ed1731a4dbbc50dbf198bef0fcf21379da1385
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-06 11:20:31 -0500

    Time: 48 ms (89.29%), Space: 49.4 MB (31.31%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.js
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.js
@@ -3,15 +3,20 @@
  * @return {number}
  */
 var trailingZeroes = function(n) {
-    if(n<=4) return 0;
-    let fact = BigInt(1);
-    let i = BigInt(1);
-    for( i;i<=n;i++){
-        fact *=i;
+    // if(n<=4) return 0;
+    // let fact = BigInt(1), i = BigInt(1);
+    // for( i;i<=n;i++) fact *=i;
+    // fact = fact+'';
+    // let count = 0;
+    // let length = fact.length-1;
+    // while(fact[length--] == 0) count++;
+    // return count;
+    let output=0;
+    while(n){
+        let temp = Math.floor(n/5);
+        output = output+temp;
+        n=temp;
+        
     }
-    fact = fact+'';
-    let count = 0;
-    let length = fact.length-1;
-    while(fact[length--] == 0) count++;
-    return count;
+    return output;
 };

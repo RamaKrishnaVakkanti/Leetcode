2e8cd3b33e488fbbfbf9ff3d0bce71f2bb3dd9bf
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-06 13:30:15 -0500

    Time: 57 ms (45.42%), Space: 49.5 MB (40.74%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -5,13 +5,13 @@
 var maxProfit = function(prices) {
     let start = prices[0];
     let profit = 0;
-    
-    for(let i=1;i<prices.length;i++){
+    let length = prices.length;
+    for(let i=1;i<length;i++){
         if(prices[i]<prices[i-1]){
             profit +=prices[i-1]-start;
             start= prices[i];
         }
     }
-    profit = prices.at(-1)>start? profit+prices.at(-1)-start: profit;
+    profit = prices[length-1]>start? profit+prices[length-1]-start: profit;
     return (profit)
 };

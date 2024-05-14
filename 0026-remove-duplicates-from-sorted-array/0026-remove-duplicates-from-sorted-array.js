1998dc3f6fd86212bdb133eee03570728f794c4e
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-13 20:53:40 -0500

    Time: 69 ms (39.53%), Space: 52.1 MB (61.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -8,45 +8,7 @@ var removeDuplicates = function(nums) {
     if(nums.length<=1) return nums.length;
     for(let i=1;i<nums.length;i++){
         if(nums[i]!==nums[i-1]) nums[count++] = nums[i];
-    }
-    
+    }    
     return count;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-//     let temp = [...new Set(nums)]; 
-
-//     let count =0;
-//     temp.forEach((value)=>{
-//         nums[count++] = value;
-//     });
-//     return temp.length;
 };

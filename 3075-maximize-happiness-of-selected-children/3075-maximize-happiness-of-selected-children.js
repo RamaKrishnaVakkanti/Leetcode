1bfab8aa1e95739cbb707360a515e7e36702a2e8
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-15 18:39:36 -0500

    Time: 714 ms (5.01%), Space: 74.1 MB (7.66%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} happiness
+ * @param {number} k
+ * @return {number}
+ */
+var maximumHappinessSum = function(happiness, k) {
+    happiness.sort((a,b)=>a-b);
+    let output=0;
+    let count=0;
+    let select = true;
+    let index= happiness.length-1;
+    while(select){
+        if(count<k && happiness[index]-count>0){
+            console.log(index);
+            output= output+ happiness[index]-count;
+            index--;
+            count++;
+        }else{
+            select =false;
+        }
+    }
+    return output;
+};

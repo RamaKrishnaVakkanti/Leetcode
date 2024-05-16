e17acd47c07d0c17bd4760cc05a047ef1f7710c2
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-16 14:04:57 -0500

    Time: 91 ms (27.66%), Space: 60.2 MB (41.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.js
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -3,17 +3,8 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    let map= {};
-    
-    for(let i=0;i<nums.length;i++) map[nums[i]]=1;
-    
-    let i=1;
-    
-    while(map[i]){
-        i++;
-    }
+    let map= {},i=1;
+    for(let i=0;i<nums.length;i++) map[nums[i]]=1;    
+    while(map[i]) i++;
     return i;
-    // for(let i=1;i<=nums.length;i++){
-    //     if(!map[i]) return i;
-    // }
 };

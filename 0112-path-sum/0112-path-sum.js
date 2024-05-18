744f5f7a249c40049afc594eefca7fb99f29d519
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-17 23:26:22 -0500

    Time: 60 ms (63.99%), Space: 52 MB (42.66%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -1,26 +1,10 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} targetSum
- * @return {boolean}
- */
+
 var hasPathSum = function(root, targetSum) {
         if(!root) return false;
     const traverse=(root, sum=0) => {
-        sum=sum+root.val;
-        
-        if(sum==targetSum && root.left == null && root.right == null){
-            return true;
-        }            
-            return ((root.left !== null ? traverse(root.left, sum) : false)||(root.right !== null ? traverse(root.right, sum) : false))
-           
+        sum=sum+root.val;  
+        if(sum==targetSum && root.left == null && root.right == null) return true;           
+        return ((root.left !== null ? traverse(root.left, sum) : false)||(root.right !== null ? traverse(root.right, sum) : false));   
     }
-    return traverse(root) || false;
+    return traverse(root);
 };

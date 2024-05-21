a6b1042003ad4e9d73f3d93819c0772318cda66a
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-21 14:14:05 -0500

    Time: 48 ms (87.85%), Space: 48.7 MB (85.05%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var findSubarrays = function(nums) {
-    let map={}; 
+    let set= new Set(); 
     for(let i=0;i<nums.length-1;i++){
         let sum=nums[i]+nums[i+1];
-        if(map[sum]) return true;
-        map[sum] = 1;
+        if(set.has(sum)) return true;
+        set.add(sum);
     }
     return false;
 };

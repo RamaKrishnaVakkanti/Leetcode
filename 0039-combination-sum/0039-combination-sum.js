3ed396548c060d83a3ccb70db4831c4d694e4e47
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-22 23:56:09 -0500

    Time: 78 ms (48.36%), Space: 57.6 MB (21.84%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -5,30 +5,24 @@
  */
 var combinationSum = function(candidates, target) {
     
-//     const result = {}
-//     const permut = (values, sum)=>{
-//         for(let i=0;i<candidates.length;i++){
-//             let localSum = sum+candidates[i];
-//             if(localSum==target) result[[...values,candidates[i]].sort((a,b)=>a-b).join("")]=1;
-//             else if(localSum>target) continue;
-//             else{
-//                 permut([...values,candidates[i]], localSum);
-//             }
-//         }
-//     }
-//     permut([],0);
-//     return Object.keys(result).map((str)=>str.split(""));
-    
+    // const result = {};
+    // const permut = (values, sum)=>{
+    //     for(let i=0;i<candidates.length;i++){
+    //         let localSum = sum+candidates[i];
+    //         if(localSum==target) result[[...values,candidates[i]].sort((a,b)=>a-b).join("-")]=1;
+    //         else if(localSum>target) return;
+    //         else{
+    //             permut([...values,candidates[i]], localSum);
+    //         }
+    //     }
+    // }
+    // permut([],0);
+    // return Object.keys(result).map((str)=>str.split("-"));
 
-    
-
-      const result = []
-  
-
+    const result = []
     const permut = (index=0, local =[], localSum=0) => {
         if(localSum>target) return;
         if(localSum == target) return result.push(local);
-
         for(let i=index;i<candidates.length;i++){
             permut(i,[...local,candidates[i]],localSum+candidates[i]);
         }

9d56d9f5fe6ad6fc988e4896460a835d208058d1
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-05-22 23:46:39 -0500

    Time: 410 ms (5.01%), Space: 59.3 MB (5.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -5,7 +5,25 @@
  */
 var combinationSum = function(candidates, target) {
     
-    const result = [];
+//     const result = {}
+//     const permut = (values, sum)=>{
+//         for(let i=0;i<candidates.length;i++){
+//             let localSum = sum+candidates[i];
+//             if(localSum==target) result[[...values,candidates[i]].sort((a,b)=>a-b).join("")]=1;
+//             else if(localSum>target) continue;
+//             else{
+//                 permut([...values,candidates[i]], localSum);
+//             }
+//         }
+//     }
+//     permut([],0);
+//     return Object.keys(result).map((str)=>str.split(""));
+    
+
+    
+
+      const result = []
+  
 
     const permut = (index=0, local =[], localSum=0) => {
         console.log(index, local, localSum)

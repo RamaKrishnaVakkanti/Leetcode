832b320f3db1f94d85b905c54f4fcef71066a4a7
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-03 20:22:26 -0500

    Time: 70 ms (65.57%), Space: 54.2 MB (25.64%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {   
+    console.log(root);
+    let output = [];
+    if(!root) return [];
+    let traverse = (root)=>{
+        output.push(root.val);
+        for(let node of root.children){
+            traverse(node);
+        }
+    }
+    traverse(root);
+    return output;
+    
+};

6c006ffbea2c7aac0a51caab1ade721b33f88d6e
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-09 22:53:29 -0500

    Time: 128 ms (8.30%), Space: 52 MB (85.47%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.js
+++ 0506-relative-ranks/0506-relative-ranks.js
@@ -13,11 +13,7 @@ var findRelativeRanks = function(score) {
     }
     
     let output = new Array(score.length).fill(0);
-    console.log(output, temp);
     
-    for(let i=0;i<score.length;i++){
-        console.log(temp.indexOf(score[i]),temp.indexOf(score[i])>2, temp.indexOf(score[i])+1,map[temp.indexOf(score[i])+1]);
-        output[i] = temp.indexOf(score[i])>2?(temp.indexOf(score[i])+1)+"" : map[temp.indexOf(score[i])+1];
-    }
+    for(let i=0;i<score.length;i++) output[i] = temp.indexOf(score[i])>2?(temp.indexOf(score[i])+1)+"" : map[temp.indexOf(score[i])+1];
     return output;
 };

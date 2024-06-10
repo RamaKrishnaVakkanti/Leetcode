e41d3a8f2c8e8761969eac5a06d422baaea89826
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-09 20:21:08 -0500

    Time: 67 ms (83.46%), Space: 54.4 MB (13.77%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.js
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.js
@@ -12,39 +12,43 @@
  * @return {TreeNode}
  */
 var removeLeafNodes = function(root, target) {
+    if (root.left) {
+        root.left = removeLeafNodes(root.left, target);
+    }
+    if (root.right) {
+        root.right = removeLeafNodes(root.right, target);
+    }
+    if (root.left === null && root.right === null && root.val === target) {
+        return null;
+    }
+    return root;
+}
+// var removeLeafNodes = function(root, target) {
+//     if(root.left) root.left = removeLeafNodes(root.left, target);
+//     if(root.right) root.right = removeLeafNodes(root.right, target);
+//     if(root.val == target) return null;
+    
+//     return root;
+    
 //     if(!root) return root;
-//     let traverse=(root)=>{
+//     let traverse=(arr)=>{
+//         let root = arr.at(-1);
+//         if(!root) return;
 //         if(root.left && root.left.val == target && !root.left.left && !root.left.right){
 //             root.left = null;
+//             arr.pop();
+//             traverse(arr);
 //         }
-        
 //         if(root.right && root.right.val == target && !root.right.left && !root.right.right){
 //             root.right = null;
+//             arr.pop();
+//             traverse(arr);
 //         }
-//         if(root.left) traverse(root.left);
-//         if(root.right) traverse(root.right);
+//         if(root.left) traverse([...arr, root.left]);
+//         if(root.right) traverse([...arr, root.right]);
 //     }
-//     traverse(root);
+//     traverse([root]);
+//     if(root.val == target && !root.left && !root.right) root = null;
 //     return (root);
     
-    if(!root) return root;
-    let traverse=(arr)=>{
-        let root = arr.at(-1);
-        if(!root) return;
-        if(root.left && root.left.val == target && !root.left.left && !root.left.right){
-            root.left = null;
-            arr.pop();
-            traverse(arr);
-        }
-        if(root.right && root.right.val == target && !root.right.left && !root.right.right){
-            root.right = null;
-            arr.pop();
-            traverse(arr);
-        }
-        if(root.left) traverse([...arr, root.left]);
-        if(root.right) traverse([...arr, root.right]);
-    }
-    traverse([root]);
-    if(root.val == target && !root.left && !root.right) root = null;
-    return (root);
-};
+// };

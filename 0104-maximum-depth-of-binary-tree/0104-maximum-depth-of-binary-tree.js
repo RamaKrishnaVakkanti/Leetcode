2117eff927948f7e38c5d5e67b4439832fd5e0c6
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-17 18:49:39 -0500

    Time: 662 ms (5.07%), Space: 59 MB (5.08%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -15,5 +15,6 @@ var maxDepth = function(root) {
     
     if(!root) return 0;
     let result = Math.max(1+maxDepth(root.left),1+maxDepth(root.right));
+    console.log(result, root);
     return result;
 };

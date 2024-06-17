9df33544e21ebbbe7dd5e3b15e8c28a00c777f74
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-16 20:36:18 -0500

    Time: 70 ms (48.33%), Space: 56 MB (73.81%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -12,10 +12,7 @@
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
-    if(root==null) return null;
-    if(val==root.val){
-        return root;
-    }
+    if(root==null || val==root.val) return root;
     if(val<root.val) return searchBST(root.left,val);
     if(val>root.val) return searchBST(root.right, val);
     

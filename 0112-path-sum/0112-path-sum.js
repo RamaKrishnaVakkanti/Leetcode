b7fb908cb701bafe23a97c00ea8959106e642271
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-17 20:12:41 -0500

    Time: 251 ms (5.04%), Space: 58.7 MB (5.09%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -1,10 +1,61 @@
 
 var hasPathSum = function(root, targetSum) {
-        if(!root) return false;
-    const traverse=(root, sum=0) => {
-        sum=sum+root.val;  
-        if(sum==targetSum && root.left == null && root.right == null) return true;           
-        return ((root.left !== null ? traverse(root.left, sum) : false)||(root.right !== null ? traverse(root.right, sum) : false));   
-    }
-    return traverse(root);
+    if(!root) return false;
+    console.log(root, targetSum);
+    targetSum = targetSum-root.val;
+    if(!root.left && !root.right && targetSum == 0) return true;
+    
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //     if(!root) return false;
+    // const traverse=(root, sum=0) => {
+    //     sum=sum+root.val;  
+    //     if(sum==targetSum && root.left == null && root.right == null) return true;           
+    //     return ((root.left !== null ? traverse(root.left, sum) : false)||(root.right !== null ? traverse(root.right, sum) : false));   
+    // }
+    // return traverse(root);
 };

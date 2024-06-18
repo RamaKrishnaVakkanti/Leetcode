53cd57f6a17cc16a4f0664aa6756249cb3c8597a
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-17 19:54:23 -0500

    Time: 60 ms (15.48%), Space: 48 MB (98.71%) - LeetHub

---
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function(root) {
+    let lowest = [Infinity, null];
+    let secondLowest = [Infinity, null];
+    
+    let traverse=(root)=>{
+        if(!root)return;
+        
+        if(root.val<lowest[0]) {
+            secondLowest = [...lowest];
+            lowest = [root.val, root];
+        }else if(root.val<secondLowest[0] && root.val!==lowest[0]){
+            secondLowest=[root.val, root];
+        }
+        traverse(root.left);
+        traverse(root.right);
+    }
+    traverse(root);
+    console.log(lowest, secondLowest);
+    return secondLowest[1] ? secondLowest[0] : -1;
+};

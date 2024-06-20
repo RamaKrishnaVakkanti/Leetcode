ded18f3fe6e1637742216c5c26b12a0bc0160bb6
RamaKrishnaVakkanti <95033115+RamaKrishnaVakkanti@users.noreply.github.com>
2024-06-20 16:43:17 -0500

    Time: 128 ms (16.89%), Space: 61.4 MB (91.80%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,25 +2,42 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var productExceptSelf = function(nums) {
+    
+//     let product=1;
+//     let Zeros= 0;
+    
+//     for(let i=0;i<nums.length;i++){
+//         if(nums[i]!==0)  product *=nums[i];
+//         else Zeros++;
+//     }
+    
+//     if(Zeros>=2) return new Array(nums.length).fill(0);
+//     let output=[];
+//     for(let i=0;i<nums.length;i++){
+//         if(Zeros){
+//             if(nums[i]==0) output.push(product);
+//             else output.push(0);
+//         }else{
+//             output.push(product/nums[i]);
+//         }
+//     }
+//     return output;
+// };
+
 var productExceptSelf = function(nums) {
-    
-    let product=1;
-    let Zeros= 0;
-    
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]!==0)  product *=nums[i];
-        else Zeros++;
+    var output = [];
+    var leftMult = 1;
+    var rightMult = 1;
+    for (var i=nums.length - 1; i >= 0; i--) {
+        output[i] = rightMult;
+        rightMult *= nums[i];
     }
-    
-    if(Zeros>=2) return new Array(nums.length).fill(0);
-    let output=[];
-    for(let i=0;i<nums.length;i++){
-        if(Zeros){
-            if(nums[i]==0) output.push(product);
-            else output.push(0);
-        }else{
-            output.push(product/nums[i]);
-        }
+    // console.log(output, rightMult);
+    for (var j=0; j < nums.length; j++) {
+        output[j] *= leftMult;
+        leftMult *= nums[j];
     }
+    // console.log(output, leftMult);
     return output;
 };
